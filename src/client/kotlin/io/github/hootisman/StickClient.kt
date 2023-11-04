20bdcd4d901760deab98523b4a18adaac0a25e54
hootisman <david1zalewski@gmail.com>
2023-11-03 23:43:43 -0400

    fabric template

---
+++ src/client/kotlin/io/github/hootisman/StickClient.kt
@@ -0,0 +1,9 @@
+package io.github.hootisman
+
+import net.fabricmc.api.ClientModInitializer
+
+object StickClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/io/github/hootisman/StickDataGenerator.kt
@@ -0,0 +1,9 @@
+package io.github.hootisman
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object StickDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
