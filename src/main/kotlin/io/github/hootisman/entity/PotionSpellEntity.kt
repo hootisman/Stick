d5926071ea070f3327b48d57eceab1a4afe369fe
hootisman <david1zalewski@gmail.com>
2023-11-11 12:42:17 -0500

    added impact particles to spell entity

--- src/main/kotlin/io/github/hootisman/entity/PotionSpellEntity.kt
+++ src/main/kotlin/io/github/hootisman/entity/PotionSpellEntity.kt
@@ -1,6 +1,8 @@
 package io.github.hootisman.entity
 
 import com.mojang.logging.LogUtils
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.LivingEntity
@@ -12,19 +14,18 @@ import net.minecraft.entity.projectile.ProjectileEntity
 import net.minecraft.entity.projectile.ProjectileUtil
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.nbt.NbtElement
+import net.minecraft.particle.ParticleEffect
 import net.minecraft.particle.ParticleTypes
 import net.minecraft.potion.Potion
 import net.minecraft.potion.PotionUtil
 import net.minecraft.potion.Potions
 import net.minecraft.registry.Registries
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.hit.EntityHitResult
 import net.minecraft.util.hit.HitResult
-import net.minecraft.util.math.MathHelper
 import net.minecraft.util.math.Vec3d
-import net.minecraft.world.RaycastContext
 import net.minecraft.world.World
-import kotlin.math.atan2
 
 class PotionSpellEntity(entityType: EntityType<out ProjectileEntity>?, world: World?) : ProjectileEntity(entityType, world) {
     constructor(x: Double, y: Double, z: Double, world: World?) : this(HootEntityRegistry.POTION_SPELL, world) {
@@ -48,11 +49,20 @@ class PotionSpellEntity(entityType: EntityType<out ProjectileEntity>?, world: Wo
         get() = this.dataTracker.get(COLOR)
         set(color) = this.dataTracker.set(COLOR,color)
 
+    var colorRGB: Vec3d = Vec3d.ZERO //does set function, will NOT be Vec3d.ZERO
+        set(value) {
+            field = Vec3d(
+                (this.colorRef shr 16 and 0xFF).toDouble() / 255.0f,
+                (this.colorRef shr 8 and 0xFF).toDouble() / 255.0f,
+                (this.colorRef shr 0 and 0xFF).toDouble() / 255.0f
+            )
+        }
     var potion: Potion
 
     init {
         this.potion = Potions.POISON
         this.initColor()
+        this.updateColorRGB()
     }
 
     override fun initDataTracker() = this.dataTracker.startTracking(COLOR,DEFAULT_COLOR)
@@ -76,31 +86,63 @@ class PotionSpellEntity(entityType: EntityType<out ProjectileEntity>?, world: Wo
     private fun isThereColor(): Boolean = this.colorRef != -1
     private fun isThereColor(nbt: NbtCompound?): Boolean? = nbt?.contains("Color", NbtElement.NUMBER_TYPE.toInt())
 
-    private fun spawnParticles(amt: Int){
-        if (!this.isThereColor() && amt <= 0) return
+    /**
+     * Updates colorRGB using its custom setter
+     *
+     * WILL NOT BE EQUAL TO Vec3d.ZERO!
+     */
+    private fun updateColorRGB() {
+        this.colorRGB = Vec3d.ZERO
+    }
 
-        val colorX: Double = (this.colorRef shr 16 and 0xFF).toDouble() / 255.0f
-        val colorY: Double = (this.colorRef shr 8 and 0xFF).toDouble() / 255.0f
-        val colorZ: Double = (this.colorRef shr 0 and 0xFF).toDouble() / 255.0f
-        for (j in 0 until amt) {
-            world.addParticle(
-                ParticleTypes.ENTITY_EFFECT,
-                getParticleX(0.5), this.randomBodyY, getParticleZ(0.5),
-                colorX, colorY, colorZ
-            )
+    /**
+     * returns darker color of this.colorRGB
+     */
+    private fun getDarkerColor(): Vec3d{
+        val ratio = 0.3
+        val diff: Vec3d = Vec3d(
+            ((this.colorRef) shr 16 and 0xFF) * ratio / 255.0,
+            ((this.colorRef) shr 8 and 0xFF) * ratio / 255.0,
+            ((this.colorRef) shr 0 and 0xFF) * ratio / 255.0
+        )
+        return this.colorRGB.subtract(diff)
+    }
+    private fun spawnPotionParticles(amt: Int){
+        spawnPotionParticles(amt, ParticleTypes.ENTITY_EFFECT, getParticleX(0.5), this.randomBodyY - 0.5, getParticleZ(0.5))
+    }
+    private fun spawnPotionParticles(amt: Int, type: ParticleEffect, x: Double, y: Double, z: Double){
+        if (!this.isThereColor() && amt <= 0) return
+        updateColorRGB()
+
+        for (j in 0 until amt) world.addParticle(type, x, y, z, this.colorRGB.x, this.colorRGB.y, this.colorRGB.z)
+
+    }
+
+    /**
+     * only ran on server :)
+     */
+    private fun spawnImpactParticles(amt: Int,x: Double,y: Double,z: Double){
+        this.updateColorRGB()
+        val darkerRGB = this.getDarkerColor()
+
+        LogUtils.getLogger().info("darker color: $darkerRGB regular color: ${this.colorRGB}")
+        for (i in 0 until amt){
+            (this.world as ServerWorld).spawnParticles(ParticleTypes.ENTITY_EFFECT,
+                x,y,z, 0, darkerRGB.x, darkerRGB.y, darkerRGB.z, 1.0)
         }
     }
 
     override fun onEntityHit(entityHitResult: EntityHitResult?) {
         super.onEntityHit(entityHitResult)
         val entity: Entity? = entityHitResult?.entity
-        LogUtils.getLogger().info("hit something!")
+//        LogUtils.getLogger().info("hit something!")
 
         if (entity is LivingEntity && entity.isAffectedBySplashPotions){
-            LogUtils.getLogger().info("hit a living entity! ${entity.name} at ${entity.pos}")
-            entity.damage(damageSources?.magic(), 1.0f)
+//            LogUtils.getLogger().info("hit a living entity! ${entity.name} at ${entity.pos}")
+            entity.damage(damageSources?.magic(), 0.0f)
+            this.spawnImpactParticles(10, getParticleX(0.5), this.randomBodyY - 0.5, getParticleZ(0.5))
             this.potion.effects.forEach { effect ->
-                LogUtils.getLogger().info("adding effect $effect")
+//                LogUtils.getLogger().info("adding effect $effect")
 
                 //must create new StatusEffectInstance, otherwise it uses current instance
                 entity.addStatusEffect(StatusEffectInstance(effect.effectType,effect.duration,effect.amplifier), this.owner)
@@ -111,16 +153,14 @@ class PotionSpellEntity(entityType: EntityType<out ProjectileEntity>?, world: Wo
 
     override fun onBlockHit(blockHitResult: BlockHitResult?) {
         super.onBlockHit(blockHitResult)
+        LogUtils.getLogger().info("side: ${blockHitResult!!.side} offset: ${blockHitResult.side.vector}")
+        this.spawnImpactParticles(10,getParticleX(0.5), this.randomBodyY - 0.5, getParticleZ(0.5))
         this.kill()
-    }
-    private fun collisionCheck(newPos: Vec3d){
-        //gets EntityHitResult if collided with one, if not then gets HitResult using raycast
-        this.checkBlockCollision()
     }
     override fun tick() {
         super.tick()
         if (this.world.isClient()){
-            this.spawnParticles(2)
+            this.spawnPotionParticles(6)
             return
         }
         this.velocityDirty= true
@@ -128,7 +168,7 @@ class PotionSpellEntity(entityType: EntityType<out ProjectileEntity>?, world: Wo
 
         var hitResult = ProjectileUtil.getCollision(this, this::canHit)
         var entityHitResult = ProjectileUtil.raycast(this,this.pos,newPos,this.boundingBox.stretch(this.velocity).expand(40.0),this::canHit, 40.0)
-        LogUtils.getLogger().info("entity: ${entityHitResult?.entity} ${entityHitResult?.type}")
+
         if (entityHitResult?.entity != null) hitResult = entityHitResult
         if (hitResult.type != HitResult.Type.MISS) this.onCollision(hitResult)
         this.checkBlockCollision()
