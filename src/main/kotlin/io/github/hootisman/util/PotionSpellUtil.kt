9d6dc8ee8094937a632b146e2da627da8bf451e4
hootisman <david1zalewski@gmail.com>
2023-11-11 23:48:56 -0500

    cast on self impl

--- src/main/kotlin/io/github/hootisman/util/PotionSpellUtil.kt
+++ src/main/kotlin/io/github/hootisman/util/PotionSpellUtil.kt
@@ -1,6 +1,7 @@
 package io.github.hootisman.util
 
 import io.github.hootisman.entity.PotionSpellEntity
+import net.minecraft.entity.effect.StatusEffectInstance
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.potion.PotionUtil
@@ -10,6 +11,9 @@ import net.minecraft.sound.SoundEvents
 object PotionSpellUtil {
     fun castOnSelf(player: PlayerEntity,stickStack: ItemStack){
         player.world?.playSound(null, player.blockPos, SoundEvents.ENTITY_ENDER_EYE_DEATH, SoundCategory.PLAYERS, 1.0f, 1.0f)
+        PotionUtil.getPotion(stickStack).effects.forEach {
+            effect -> player.addStatusEffect(StatusEffectInstance(effect.effectType, effect.duration, effect.amplifier), player)
+        }
     }
     fun castOnOther(player: PlayerEntity,stickStack: ItemStack){
         player.world?.playSound(null, player.blockPos, SoundEvents.ENTITY_SHULKER_SHOOT, SoundCategory.PLAYERS, 1.0f, 1.0f)
